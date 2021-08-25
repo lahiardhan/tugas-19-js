42b1179bc9b4df94217de84a5503219372e3d77b
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 17:00:04 +0700

    Tugas 19 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+let string1 = "Semangat..." ;
+console.log(string1.repeat(5));
+
+console.log("");
+
+console.log("*".repeat(5));
+console.log("*".repeat(25));
+console.log("*".repeat(45));
+console.log("*".repeat(90));
+console.log("*".repeat(45));
+console.log("*".repeat(25));
+console.log("*".repeat(5));
